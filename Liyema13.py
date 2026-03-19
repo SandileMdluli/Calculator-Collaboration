45cf3db478d6f838c6d225bc58e4a09d423082bc
liyema13 <20260970@ctucareer.co.za>
2026-03-19 12:38:48 +0200

    Create Liyema13.py

---
+++ Liyema13.py
@@ -0,0 +1,15 @@
+def multiply(a,b):
+    c = a * b
+    return c
+
+def add(a,b):
+    c = a + b
+    return 
+
+def subtraction(a,b):
+    c = a * b
+    return c
+
+def addition(a,b):
+    c = a + b
+    return c
