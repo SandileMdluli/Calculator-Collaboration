8c343ae930e51e1cb9846f0f6bfb4eec7b9528ae
liyema13 <20260970@ctucareer.co.za>
2026-03-19 12:57:42 +0200

    Create Calculator 2.py

---
+++ Calculator 2.py
@@ -0,0 +1,30 @@
+#Team Members
+#Sandile, Liyema, Mbuso
+
+greet = "Welcome to THE Calculator"
+print(greet)
+
+number_1 = float(input("Enter a number:"))
+number_2 = float(input("Enter a number:"))
+
+choose = int(input("What operation do you want to use"))\
+
+def multiply(a,b):
+    c = a * b
+    return c
+
+def division(a,b):
+    c = a / b
+    return c
+
+def subtraction(a,b):
+    c = a * b
+    return c
+
+def addition(a,b):
+    c = a + b
+    return c
+
+def modulas(a,b):
+    c = a % b
+    return c
