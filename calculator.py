4a3fac12fce155d1a2c38624a0c959bef75c06e5
liyema13 <20260970@ctucareer.co.za>
2026-03-19 12:29:59 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -9,3 +9,18 @@ number_2 = float(input("Enter a number:"))
 
 choose = int(input("What operation do you want to use"))\
 
+def multiply(a,b):
+    c = a * b
+    return c
+
+def add(a,b):
+    c = a + b
+    return 
+
+def subtraction(a,b):
+    c = a * b
+    return c
+
+def addition(a,b):
+    c = a + b
+    return c
