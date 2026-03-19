3bddb70b21a200074098f55be85608a59ae2973b
liyema13 <20260970@ctucareer.co.za>
2026-03-19 12:25:50 +0200

    Update liyema.py

---
+++ liyema.py
@@ -0,0 +1,17 @@
+
+def multiply(a,b):
+    c = a * b
+    return c
+
+def add(a,b):
+    c = a + b
+    return 
+
+def subtraction(a,b):
+    c = a * b
+    return c
+
+def addition(a,b):
+    c = a + b
+    return c
+
