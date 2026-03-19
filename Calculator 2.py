b5e8abc179279c95b73e8fa6f129bf47f5e8de73
SandileMdluli <iamsandile17@gmail.com>
2026-03-19 13:38:35 +0200

    Update Calculator 2.py

--- Calculator 2.py
+++ Calculator 2.py
@@ -1,36 +1,45 @@
 #Team Members
 #Sandile, Liyema, Mbuso
 
+#Sandile did the Inputs
 greet = "Welcome to THE Calculator"
 print(greet)
 
+#User enters number
 num1 = float(input("Enter a number:"))
 num2 = float(input("Enter a number:"))
 
+#User chooses what operator they want to enter
 choose = int(input("What operation do you want to use"))
 
+#Liyema did the menu to define the operators
+#Mutiplication funtctio
 def multiply(a,b):
     c = a * b
     return c
 
+#Division function
 def division(a,b):
     c = a / b
     return c
 
+#Subtraction function
 def subtraction(a,b):
     c = a * b
     return c
 
+#Addition Function
 def addition(a,b):
     c = a + b
     return c
 
+#Modulas function
 def modulas(a,b):
     c = a % b
     return c
 
+#Mbuso did IF to operate the calculator
 if choose == 1:
-<<<<<<< HEAD
     print(multiply(num1,num2))
 elif choose ==2:
     print(division(num1,num2))
@@ -40,6 +49,3 @@ elif choose ==4:
     print(addition(num1,num2)) 
 elif choose ==5:
     print(modulas(num1, num2))
-=======
-     
->>>>>>> 2e3531eff5759b883774d3a87b5c2c5fd838ff68
